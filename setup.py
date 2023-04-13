43b98e9c05f6aa2728733dbbcb8f430e5de12944
Micha Gorelick <mynameisfiber@gmail.com>
2023-04-13 18:51:13 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="showsnearme",
-    version="0.6.0",
+    version="0.7.0",
     description="Using the ohmyrockness to bring shows to your CLI",
     url="http://github.com/mynameisfiber/showsnearme",
     author="Micha Gorelick",
